48735033eb9d6fd3e85f4bd769ec5e3c3508f0fb
Izabela Gaspari <belaeive@gmail.com>
2025-03-15 20:36:06 -0300

    components dinamycs propertys

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { MenuTittleComponent } from "../../components/menu-tittle/menu-tittle.component";
+import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { SmallCardComponent } from "../../components/small-card/small-card.component";
 import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 
+
 @Component({
   selector: 'app-home',
-  imports: [MenuTittleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent],
+  imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
