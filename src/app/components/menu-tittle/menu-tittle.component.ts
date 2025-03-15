02f721d5d7aab71e84ef72fb5c8d8fb280d7389b
Izabela Gaspari <belaeive@gmail.com>
2025-03-15 19:16:47 -0300

    components createds with styles

---
+++ src/app/components/menu-tittle/menu-tittle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-tittle',
+  imports: [],
+  templateUrl: './menu-tittle.component.html',
+  styleUrl: './menu-tittle.component.css'
+})
+export class MenuTittleComponent {
+
+}
