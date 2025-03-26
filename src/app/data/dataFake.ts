19bf0b5ccdf5d00e3e649a5ee0f59438b54bec1f
Izabela Gaspari <belaeive@gmail.com>
2025-03-25 23:47:53 -0300

    Alterando layout

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -5,11 +5,11 @@ export const dataFake = [
     "description": `
       Tenho 25 anos e iniciei minha trajetória acadêmica na área de Engenharia, mas, ao longo do tempo,
       descobri meu interesse pela tecnologia. Atualmente, estou focada no desenvolvimento de habilidades
-      em dados que me despertam grande interesse.
+      em dados o que me desperta grande fascínio.
 
-      Esse bootcamp representou um grande desafio para mim, pois, até então, minha experiência com
+      O bootcamp DecolaTech representou um grande desafio para mim, pois, até então, minha experiência com
       programação se limitava ao básico. No entanto, ao longo do processo, desenvolvi um interesse nessa
-      parte de programação. Apesar de não saber muita coisa, fiquei curiosa para aprender mais sobre.
+      parte de programação. Apesar de não obter tanto conhecimento na área, fiquei curiosa para aprender mais sobre o assunto.
 
       Sou uma pessoa curiosa, determinada, resiliente e com grande capacidade de aprendizado contínuo.
       Estou em busca de oportunidades para aplicar e expandir meus conhecimentos em análise de dados,
@@ -21,28 +21,28 @@ export const dataFake = [
     "id":"2",
     "title": "Minha formação:",
     "description": `Concluí minha primeira graduação em Análise e Desenvolvimento de Sistemas pela Fundação Santo André em dezembro de 2024.
-    Atualmente, estou cursando minha segunda graduação em Ciência de Dados e Inteligência Artificial, com previsão de conclusão em dezembro de 2027.
-    Busco aprofundar meus conhecimentos em análise de dados, aprendizado de máquina e inteligência artificial, com o objetivo de aplicar soluções
-    inovadoras e baseadas em dados para tomada de decisões estratégicas. `,
+    Atualmente, estou cursando minha segunda graduação em Ciência de Dados e Inteligência Artificial, com previsão de conclusão para dezembro de 2027.
+    Busco aprofundar meus conhecimentos em análise e manipulação de dados, com o objetivo de aplicar soluções
+    inovadoras, baseadas em dados para tomada de decisões estratégicas. `,
     "photoCover": "foto apresentacao.jpeg"
   },
   {
     "id":"3",
     "title": "Minha experiência:",
-    "description": `Atualmente, atuo na área Dados, em testes, validação e correção de scripts no Azure, além de elaborar planilhas para monitoramento e verificação de atividades.
-    Tenho experiência na criação e manutenção de pipelines e processos de ingestão de dados, alteração de objetos de banco de dados, execução de pipelines e resolução de
-    problemas técnicos. Anteriormente, trabalhei na área de infraestrutura no SEBRAE, auxiliando colaboradores, gerenciando chamados, criando planilhas de controle para
-    auditoria e desenvolvendo procedimentos técnicos. Também atuei na configuração e manutenção de telefonia, realizando alterações de ramais e ajustes em equipamentos. `,
+    "description": `Atualmente, atuo na área de Dados, realizando testes, validação e correção de scripts no Azure, além de criar planilhas para o monitoramento
+     e verificação das atividades. Tenho experiência na criação e manutenção de pipelines, na ingestão de dados e na alteração de objetos de banco de dados,
+     além de executar pipelines e resolver problemas técnicos. Anteriormente, trabalhei na área de infraestrutura no SEBRAE, onde auxiliei colaboradores, gerenciei
+     chamados, criei planilhas de controle para auditoria e desenvolvi procedimentos técnicos. Também atuei na configuração e manutenção de telefonia, realizando alterações de ramais e ajustes em equipamentos. `,
     "photoCover": "https://hupdata.com/wp-content/uploads/2020/03/17.03.png"
   },
   {
     "id":"4",
-    "title": "Minha experiência:",
-    "description": `Atualmente, atuo na área Dados, em testes, validação e correção de scripts no Azure, além de elaborar planilhas para monitoramento e verificação de atividades.
-    Tenho experiência na criação e manutenção de pipelines e processos de ingestão de dados, alteração de objetos de banco de dados, execução de pipelines e resolução de
-    problemas técnicos. Anteriormente, trabalhei na área de infraestrutura no SEBRAE, auxiliando colaboradores, gerenciando chamados, criando planilhas de controle para
-    auditoria e desenvolvendo procedimentos técnicos. Também atuei na configuração e manutenção de telefonia, realizando alterações de ramais e ajustes em equipamentos. `,
-    "photoCover": "https://cdn.cookielaw.org/logos/bf278563-ac10-417f-8f6f-7b51998dc338/acadaa94-4723-4fc6-9d15-0d7316561595/7c1243eb-e96c-4d72-b8b3-7e676efd5e1f/AvanadeLogoNoTM_AWColor_RGB.png"
+    "title": "O que o DecolaTech me trouxe de aprendizado?",
+    "description": `Durante o bootcamp, percebi que a programação não é tão complexa quanto eu imaginava, o que despertou meu interesse por essa área – algo que eu nunca havia considerado antes. Ao longo do programa,
+     tive a chance de aprender linguagens de programação e ferramentas que são amplamente utilizadas no mercado, além de práticas fundamentais para o desenvolvimento de soluções. Através de projetos práticos, pude aplicar
+     esses conhecimentos de forma eficaz, resolver problemas reais e desenvolver uma abordagem mais estruturada para a criação de códigos. Essas novas habilidades me ajudaram a expandir meu conjunto de competências e a
+     enriquecer meu currículo, me preparando para enfrentar desafios profissionais de forma mais confiante.`,
+    "photoCover": "https://hermes.dio.me/tracks/159b54d1-bbf4-4f72-a717-69398a7aa683.png"
   }
 
 ]
